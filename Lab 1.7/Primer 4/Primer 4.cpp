12a3e02de12e6bce727a78429fc6141f34ccac1e
fulloffacid <55211326+fulloffacid@users.noreply.github.com>
2019-10-09 16:24:47 +0300

    Rename Primer 4.cpp to Lab 1.7/Primer 4/Primer 4.cpp

---
+++ Lab 1.7/Primer 4/Primer 4.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "pch.h"
+#include <stdio.h>
+
+int sum(int x[])
+{
+	int  res = 0;
+	for (int i = 0; i < 5; i++)
+		res += x[i];
+	return res;
+}
+
+int main()
+{
+	int mas[3][5], i, j;
+	for (i = 0;i < 3;i++)
+		for (j = 0;j < 5;j++)
+			mas[i][j] = i * j;
+	for (i = 0;i < 3;i++)
+		printf("\n res=%d\n", sum(mas[i]));
+	for (i = 0;i < 3;i++) {
+		for (j = 0;j < 5;j++)
+			printf("%d ", mas[i][j]);
+		printf("\n");
+	}
+}
