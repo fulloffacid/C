016983d97df0d89875c33496efcf4a639052be03
fulloffacid <55211326+fulloffacid@users.noreply.github.com>
2019-10-09 16:19:47 +0300

    Rename Primer 1.cpp to Lab 1.7/Primer 1.cpp

---
+++ Lab 1.7/Primer 1.cpp
@@ -0,0 +1,19 @@
+﻿// Primer 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "pch.h"
+#include <stdio.h>
+
+int square(int y); //прототип функции
+int _tmain(int argc, char* argv[])
+{
+	int x = 0;
+	for (x = 1;x <= 10;x++) 
+	printf_s("%d \n",square(x));
+return 0;
+}
+//описание функции
+int square(int y)
+{
+	return y * y;
+}
+
