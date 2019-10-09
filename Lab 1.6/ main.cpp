4aefe7ec0581055739faff45401d1c06cc78e75d
fulloffacid <55211326+fulloffacid@users.noreply.github.com>
2019-10-09 16:09:19 +0300

    Rename Laba 6.1 (ispr).cpp to Lab 1.6/ main.cpp

---
+++ Lab 1.6/ main.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+
+int main()
+{
+	int *mas, i = 0, k=0, kol = 0, sum = 0, i_i = 0;
+	do {
+		printf_s("Vvedite kol-vo elementov massiva\n");
+		scanf_s("%d", &k);
+	} while (k >= 10);
+	mas = new int[k];
+	if (mas == NULL) {
+		printf_s("Error");
+		return 0;
+	}
+
+	for (i = 0;i < k;i++) {
+		printf_s("Vvedite element [%d]\n", i + 1);
+		scanf_s("%i", mas + i);
+	}
+	for (i = 0;i < k;i++) {
+		if (*(mas + i) > 0) kol++;
+	}
+	printf_s("Kol-vo polojitel'nih chisel %d\n", kol);
+
+	for (i = 0;i < k;i++)
+		if (*(mas + i) == 0) i_i = i;
+
+	for (i = i_i;i < k;i++) {
+		sum += *(mas + i);
+	}
+	printf_s("Sum =  %d\n", sum);
+	delete[]mas;
+
+	return 0;
+}
