dfe19a6ae483dc3cf99678429b486788c0850569
fulloffacid <55211326+fulloffacid@users.noreply.github.com>
2019-10-09 19:52:48 +0300

    Add files via upload

---
+++ Rekursia.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+double factorial(double n);
+double summa(double n);
+
+double factorial(double n)
+{
+	if (n == 0) 
+		return 1;
+	else 
+		return n * factorial(n - 1);
+}
+
+double summa(double n) {
+	if (n == 0)
+		return 1;
+	else
+		return 1 / factorial(n);
+}
+
+int main()
+{
+	int n, i;
+	double sum = 0;
+	cout << "Enter the number:";
+	cin >> n;
+	for (i = 0; i <= n; i++)
+		sum += summa(i);
+
+	cout <<"The amount of row"<<endl;
+	cout << sum;
+	return 0;
+}
---
+++ Rekursia2.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int chislo(int n) {
+	//int max = 0;
+	if (n < 10)
+	{
+		//if (max < n) max = n;
+		return n;
+	}
+	else //{
+		//if (max < chislo(n / 10)){ max = chislo(n / 10);
+		return chislo(n / 10);
+	//}
+		//else return chislo(n / 10);
+}
+
+
+int main()
+{
+	int number = 0;
+	do {
+		cout << "Enter the number: " << endl;
+		cin >> number;
+		if (number <= 0) cout <<"Enter positive value"<< endl;
+		else
+		{
+			cout << "The senior level: " << endl;
+			cout << chislo(number);
+		}
+	} while (number < 0);
+	return 0;
+}
